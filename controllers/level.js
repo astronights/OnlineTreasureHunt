31f7e4d08fc57394a53eee2e93baa108daf79bd0
astronights <shubhankar.a31@gmail.com>
2020-06-01 20:05:16 +0800

    update level schema

--- controllers/level.js
+++ controllers/level.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
+var Level = require('../models/Level');
 
 module.exports = (req, res) => {
-  res.sendFile(path.join(__dirname, "../views/level.html"));
+  res.sendFile(path.join(__dirname, "../views/level.ejs"));
 }
--- models/Level.js
+++ models/Level.js
@@ -2,9 +2,13 @@ const mongoose = require('../db');
 
 const userSchema = mongoose.Schema({
     level_num: Number,
+    level_key: String,
     level_text: [String],
     level_images: [String],
-    level_times: [Date],
+    level_times: [{
+      user_id: String,
+      user_time: Date,
+    }],
     num_solved: Number,
     level_clues: [String]
 });
