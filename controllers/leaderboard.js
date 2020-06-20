18c90277584111b12ee372a9deb462dbdcf4c247
Raghu Saxena <poiasdpoiasd@live.com>
2020-06-20 21:46:14 +0800

    Basic improvements in leaderboard

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -1,18 +1,17 @@
-var express = require('express');
-var path = require('path');
-var mongoose = require('../db');
-var sorter = require('./utils/sorter');
-var User = require('../models/User');
+const express = require('express'),
+	path = require('path'),
+	sorter = require('./utils/sorter'),
+	User = require('../models/User');
 
 module.exports = (req, res) => {
-  User.find({}, (err, users) => {
-    if(err){
-      throw err;
-    }
-    else{
-      console.log(users);
-      res.render(path.join(__dirname, "../views/leaderboard.ejs"), {users: users.sort(sorter("current_level", "level_times"))});
-    }
-  })
+	User.find({}).select('username current_level level_times').exec((err, users) => {
+		if (err) {
+			throw err;
+		}
+		else {
+			// console.log(users);
+			res.render(path.join(__dirname, "../views/leaderboard.ejs"), { users: users.sort(sorter("current_level", "level_times")) });
+		}
+	})
 
 }
