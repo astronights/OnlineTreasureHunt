0ba337a34cd6dbc83193001b9a83206469a4785c
Raghu Saxena <poiasdpoiasd@live.com>
2020-06-22 10:19:52 +0200

    Contact backend POST

---
+++ controllers/contact.js
@@ -0,0 +1,54 @@
+const Contact = require('../db/Contact');
+
+module.exports = async (req, res) => {
+
+    const { cfname, cfemail, cfmessage } = req.body;
+
+    if (!cfname || !cfemail || !cfmessage){
+        res.status(400).json({
+            error: "Bad request",
+            ip: req.ip
+        });
+
+        return;
+    }
+
+    if (typeof cfname != "string" || typeof cfemail != "string" || typeof cfmessage != "string"){
+        res.status(400).json({
+            error: "Bad request",
+            ip: req.ip
+        });
+
+        return;
+    }
+
+    if (cfname == "" || cfemail == "" || cfmessage == ""){
+        res.status(400).json({
+            error: "Bad request",
+            ip: req.ip
+        });
+
+        return;
+    }
+
+    let contact = new Contact();
+
+    contact.name = cfname;
+    contact.email = cfemail;
+    contact.message = cfmessage;
+
+    try {
+        await contact.save();
+
+        // req.session
+        res.redirect('/home');
+        return;
+    } catch (e){
+        res.status(500).json({
+            error: "Failed to save to DB",
+            ip: req.ip
+        });
+
+        return;
+    }
+}
--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ var upload = require('./storage');
 
 var signup = require('./controllers/signup');
 var login = require('./controllers/login');
+const contact = require('./controllers/contact');
 
 var home = require('./controllers/home');
 var level = require('./controllers/level');
@@ -18,19 +19,19 @@ var error = require('./controllers/error');
 var addlevelget = require('./controllers/addlevelget');
 var addlevelpost = require('./controllers/addlevelpost');
 
-router.get('/error', error);
+// router.get('/error', error);
 router.get('/home', home);
-router.get('/levels/:name', level);
+// router.get('/levels/:name', level);
 router.get('/leaderboard', leaderboard);
-router.get('/getlevel', getlevel);
-router.get('/level_data/:name', level_data);
-router.get('/level', formlevel);
-
+// router.get('/getlevel', getlevel);
+// router.get('/level_data/:name', level_data);
+// router.get('/level', formlevel);
+router.post('/contact-details', contact);
 router.post('/signup', signup);
-router.post('/login', login);
-router.post('/checklevel', checklevel);
+// router.post('/login', login);
+// router.post('/checklevel', checklevel);
 
-router.get('/addlevel', addlevelget);
-router.post('/addlevel', upload.any(), addlevelpost);
+// router.get('/addlevel', addlevelget);
+// router.post('/addlevel', upload.any(), addlevelpost);
 
 module.exports = router;
