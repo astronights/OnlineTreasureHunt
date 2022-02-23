5c29783fd931e0051eb64074e2fef0a01efc1ad4
astronights <shubhankar.a31@gmail.com>
2022-02-23 23:37:40 +0800

    Fixes

--- controllers/level.js
+++ controllers/level.js
@@ -1,106 +1,104 @@
-const path = require('path'),
-	Level = require('../models/Level'),
-	Token = require('../models/Token'),
-	User = require('../models/User');
+const path = require("path"),
+  Level = require("../models/Level"),
+  Token = require("../models/Token"),
+  User = require("../models/User");
 
 //GET /level/:name
 module.exports = async (req, res) => {
+  const level_key = req.params.name;
+  const token = req.cookies.token;
 
-	const level_key = req.params.name;
-	const token = req.cookies.token;
+  let user_token;
 
-	let level;
-	
-	try {
-		level = await Level.findOne({level_key: level_key});
-	} catch (e){
-		res.status(500).json({
-			error: 'Failed to query DB'
-		});
+  try {
+    user_token = await Token.findOne({ tokens: token });
+  } catch (e) {
+    res.status(500).json({
+      error: "Failed to query DB",
+    });
 
-		return;
-	}
+    return;
+  }
 
-	if (!level){
-		res.json({
-			error: 'Incorrect answer'
-		});
+  if (!user_token) {
+    req.session.locals = {
+      data: {
+        success: false,
+        logged_in: false,
+        message: "Invalid token",
+      },
+    };
 
-		return;
-	}
+    return res.redirect("/home");
+  }
 
-	let user_token;
+  let user;
 
-	try {
-		user_token = await Token.findOne({tokens: token});
-	} catch (e){
-		res.status(500).json({
-			error: 'Failed to query DB'
-		});
+  try {
+    user = await User.findOne({ email: user_token.email });
+  } catch (e) {
+    res.status(500).json({
+      error: "Failed to query DB",
+    });
 
-		return;
-	}
+    return;
+  }
 
-	if (!user_token){
+  if (!user) {
+    res.status(404).json({
+      error: "User not found",
+    });
 
-		req.session.locals = {
-			"data": {
-				"success": false,
-				"logged_in": false,
-				"message": "Invalid token"
-			}
-		};
+    return;
+  }
 
-		return res.redirect('/home');
-	}
+  let level;
 
-	let user;
+  try {
+    level = await Level.findOne({ level_key: level_key });
+  } catch (e) {
+    res.status(500).json({
+      error: "Failed to query DB",
+    });
 
-	try {
-		user = await User.findOne({email: user_token.email});
-	} catch (e){
-		res.status(500).json({
-			error: 'Failed to query DB'
-		});
+    return;
+  }
 
-		return;
-	}
+  if (!level) {
+    res.json({
+      error: "Incorrect answer",
+    });
 
-	if (!user){
-		res.status(404).json({
-			"error": "User not found"
-		});
+    return;
+  }
 
-		return;
-	}
+  if (user.current_level + 1 == level.level_num) {
+    //They found the answer to the next level
+    user.current_level = level.level_num;
+    user.level_times.push(Date.now());
 
-	if (user.current_level + 1 == level.level_num){
-		//They found the answer to the next level
-		user.current_level = level.level_num;
-		user.level_times.push(Date.now());
+    try {
+      await user.save();
+    } catch (e) {
+      res.status(500).json({
+        error: "Failed to query DB",
+      });
 
-		try {
-			await user.save();
-		} catch (e){
-			res.status(500).json({
-				error: 'Failed to query DB'
-			});
-	
-			return;
-		}
+      return;
+    }
 
-		res.render(path.join(__dirname, '../views/level.ejs'), { level: level});
-		return;
-	}
+    res.render(path.join(__dirname, "../views/level.ejs"), { level: level });
+    return;
+  }
 
-	if (user.current_level >= level.level_num){
-		//They are accessing a level they've already cleared. Ok buddy.
-		res.render(path.join(__dirname, '../views/level.ejs'), { level: level});
-		return;
-	}
+  if (user.current_level >= level.level_num) {
+    //They are accessing a level they've already cleared. Ok buddy.
+    res.render(path.join(__dirname, "../views/level.ejs"), { level: level });
+    return;
+  }
 
-	//If we got here, means it is a CORRECT answer, but of some future level. GG
-	res.json({
-		error: 'Incorrect answer'
-	});
-}
+  //If we got here, means it is a CORRECT answer, but of some future level. GG
+  res.json({
+    error: "Incorrect answer",
+  });
+};
