fe1b10a8a65c267fc0fe4f8b08f710ac912ac4e3
astronights <shubhankar.a31@gmail.com>
2022-02-23 22:23:24 +0800

    Updates to clean

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express'),
-	session = require('express-session'),
-	bodyParser = require('body-parser'),
-	cookieParser = require('cookie-parser'),
-	routes = require('./routes');
-	
+const express = require("express"),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  routes = require("./routes");
+
 const app = express();
-const http = require('http').Server(app);
+const http = require("http").Server(app);
 
 const mimeTypes = {
-  'text/css': ['css'],
-  'application/javascript': ['js']
-}
+  "text/css": ["css"],
+  "application/javascript": ["js"],
+};
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static("public/"));
 app.use(cookieParser());
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use("/", routes);
 
-const IP = process.env.IP || '127.0.0.1';
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 
-http.listen(PORT, IP, function(){
-    console.log(`Server started on port ${PORT}`);
+http.listen(PORT, HOST, function () {
+  console.log(`Server started on port ${PORT}`);
 });
